de40b87a0fb8d606a51bbc17ae6aa78504fff7f7
d.plushchenkov <d.plushchenkov@notamedia.ru>
2023-03-25 17:25:53 +0300

    add new order

--- src/Parser.ts
+++ src/Parser.ts
@@ -29,35 +29,122 @@ export class Parser {
             workString = this.originalString;
         }
 
-        if ((workString.match(this.validActionsGlobal) ?? []).length > 1) {
-            ['*', '/', '-', '+'].forEach((action) => {
-                if (workString.indexOf(action) !== -1) {
-                    const actionPosition = workString.lastIndexOf(action);
+        console.log('execute - ', workString);
 
-                    const leftPart = workString.slice(0, actionPosition);
+        if ((workString.match(this.validActionsGlobal) ?? []).length > 1) {
+            workString.split('').forEach((value, index) => {
+                //console.log(value, ' | ', index);
+
+                if (['+', '-'].includes(value)) {
+                    const action = value;
+
+                    const actionPosition = workString.indexOf(action);
+
+                    const leftArray = workString
+                        .slice(0, actionPosition)
+                        .split('');
+
+                    //console.log(leftArray);
+
+                    let leftPos = 0;
+                    let leftPart = leftArray.join('');
+                    let leftArgument = leftPart;
+
+                    for (let i = leftArray.length - 1; i >= 0; i--) {
+                        if (['*', '/', '-', '+'].indexOf(leftArray[i]) >= 0) {
+                            leftPos = i;
+                            break;
+                        }
+                    }
+
+                    if (leftPos !== 0) {
+                        leftPart = workString.slice(0, leftPos + 1);
+
+                        leftArgument = workString.slice(
+                            leftPos + 1,
+                            actionPosition
+                        );
+                    }
+
+                    const rightArray = workString
+                        .slice(actionPosition + 1, workString.length)
+                        .split('');
+
+                    let rightPos = 0;
+
+                    for (let i = 0; i < rightArray.length; i++) {
+                        if (['*', '/', '-', '+'].indexOf(rightArray[i]) >= 0) {
+                            rightPos = i;
+                            break;
+                        }
+                    }
 
                     const rightPart = workString.slice(
-                        actionPosition + 1,
+                        actionPosition + 1 + rightPos,
                         workString.length
                     );
 
-                    workString =
-                        this.execute(leftPart) + '' + action + '' + rightPart;
+                    const rightArgument = workString.slice(
+                        actionPosition + 1,
+                        actionPosition + 1 + rightPos
+                    );
+
+                    console.log(
+                        leftPos,
+                        ' - ',
+                        leftPart,
+                        ' | ',
+                        leftArgument,
+                        ' | ',
+                        action,
+                        ' | ',
+                        rightArgument,
+                        ' | ',
+                        rightPart
+                    );
+
+                    const result = this.execute(
+                        [
+                            leftPos === 0 ? leftPart : leftArgument,
+                            action,
+                            rightPos === 0 ? rightPart : rightArgument,
+                        ].join('')
+                    );
+
+                    // console.log('result', result);
+
+                    workString = [
+                        leftPos === 0 ? '' : leftPart,
+                        result,
+                        rightPos === 0 ? '' : rightPart,
+                    ].join('');
                 }
             });
         }
 
-        const result: RegExpMatchArray | null = workString.match(
-            this.validActions
-        );
+        // console.log(workString);
 
-        if (result === null || typeof result.index === 'undefined') {
-            throw new Error('Parser error');
+        if ((workString.match(this.validActionsGlobal) ?? []).length === 1) {
+            const result: RegExpMatchArray | null = workString.match(
+                this.validActions
+            );
+
+            if (result === null || typeof result.index === 'undefined') {
+                throw new Error('Parser error');
+            }
+
+            const action: string = workString.slice(
+                result.index,
+                result.index + 1
+            );
+
+            return this.callMakeAction(
+                action,
+                workString.split(this.validActions)
+            );
         }
 
-        const action: string = workString.slice(result.index, result.index + 1);
-
-        return this.callMakeAction(action, workString.split(this.validActions));
+        return +workString;
     };
 
     callMakeAction = (action: string, nums: Array<string>): number => {
