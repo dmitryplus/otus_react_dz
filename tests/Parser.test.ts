fdddd4be1e30472f18554a6d1250a46db2cbb7b2
d.plushchenkov <d.plushchenkov@notamedia.ru>
2023-03-25 13:32:20 +0300

    update logic for order action

--- tests/Parser.test.ts
+++ tests/Parser.test.ts
@@ -18,120 +18,132 @@ const mockCulc = (action: string, nums: Array<string>): number => {
 };
 
 describe('test parse input string: Parser module', () => {
-    test('Parser set symbol to catch Error', () => {
-        expect(() => {
-            new Parser('asdase');
-        }).toThrow();
-    });
 
-    test('Parser set empty string to catch Error', () => {
-        expect(() => {
-            new Parser('');
-        }).toThrow();
-    });
+//     test('Parser set symbol to catch Error', () => {
+//         expect(() => {
+//             new Parser('asdase');
+//         }).toThrow();
+//     });
 
-    test('Parser set only numbers to catch Error', () => {
-        expect(() => {
-            new Parser('56 76');
-        }).toThrow();
-    });
+//     test('Parser set empty string to catch Error', () => {
+//         expect(() => {
+//             new Parser('');
+//         }).toThrow();
+//     });
 
-    test('Parser set only * to catch Error', () => {
-        expect(() => {
-            new Parser('*');
-        }).toThrow();
-    });
+//     test('Parser set only numbers to catch Error', () => {
+//         expect(() => {
+//             new Parser('56 76');
+//         }).toThrow();
+//     });
 
-    test('Parser set only actions to catch Error', () => {
-        expect(() => {
-            new Parser('*++-//**');
-        }).toThrow();
-    });
+//     test('Parser set only * to catch Error', () => {
+//         expect(() => {
+//             new Parser('*');
+//         }).toThrow();
+//     });
 
-    test('Parser set wrong action "2#3" to catch Error', () => {
-        expect(() => {
-            new Parser('2#3');
-        }).toThrow();
-    });
+//     test('Parser set only actions to catch Error', () => {
+//         expect(() => {
+//             new Parser('*++-//**');
+//         }).toThrow();
+//     });
 
-    test('Parser set "2+3" to call MakeAction.execute', () => {
-        const parser = new Parser('2+3');
-        const spy = jest
-            .spyOn(parser, 'callMakeAction')
-            .mockImplementation(mockCulc);
-        expect(parser.execute()).toBe(5);
+//     test('Parser set wrong action "2#3" to catch Error', () => {
+//         expect(() => {
+//             new Parser('2#3');
+//         }).toThrow();
+//     });
 
-        spy.mockRestore();
-    });
+//     test('Parser set "2+3" to call MakeAction.execute', () => {
+//         const parser = new Parser('2+3');
+//         const spy = jest
+//             .spyOn(parser, 'callMakeAction')
+//             .mockImplementation(mockCulc);
+//         expect(parser.execute()).toBe(5);
 
-    test('Parser set "2-3" to call MakeAction.execute', () => {
-        const parser = new Parser('2-3');
-        const spy = jest
-            .spyOn(parser, 'callMakeAction')
-            .mockImplementation(mockCulc);
-        expect(parser.execute()).toBe(-1);
+//         spy.mockRestore();
+//     });
 
-        spy.mockRestore();
-    });
+//     test('Parser set "2-3" to call MakeAction.execute', () => {
+//         const parser = new Parser('2-3');
+//         const spy = jest
+//             .spyOn(parser, 'callMakeAction')
+//             .mockImplementation(mockCulc);
+//         expect(parser.execute()).toBe(-1);
 
-    test('Parser set "2*3" to call MakeAction.execute', () => {
-        const parser = new Parser('2*3');
-        const spy = jest
-            .spyOn(parser, 'callMakeAction')
-            .mockImplementation(mockCulc);
-        expect(parser.execute()).toBe(6);
+//         spy.mockRestore();
+//     });
 
-        spy.mockRestore();
-    });
+//     test('Parser set "2*3" to call MakeAction.execute', () => {
+//         const parser = new Parser('2*3');
+//         const spy = jest
+//             .spyOn(parser, 'callMakeAction')
+//             .mockImplementation(mockCulc);
+//         expect(parser.execute()).toBe(6);
 
-    test('Parser set "3/1" to call MakeAction.execute', () => {
-        const parser = new Parser('3/2');
-        const spy = jest
-            .spyOn(parser, 'callMakeAction')
-            .mockImplementation(mockCulc);
-        expect(parser.execute()).toBe(1.5);
+//         spy.mockRestore();
+//     });
 
-        spy.mockRestore();
-    });
+//     test('Parser set "3/1" to call MakeAction.execute', () => {
+//         const parser = new Parser('3/2');
+//         const spy = jest
+//             .spyOn(parser, 'callMakeAction')
+//             .mockImplementation(mockCulc);
+//         expect(parser.execute()).toBe(1.5);
 
-    test('Parser set "2 + 3" to call MakeAction.execute', () => {
-        const parser = new Parser('2 + 3');
-        const spy = jest
-            .spyOn(parser, 'callMakeAction')
-            .mockImplementation(mockCulc);
-        expect(parser.execute()).toBe(5);
+//         spy.mockRestore();
+//     });
 
-        spy.mockRestore();
-    });
+//     test('Parser set "2 + 3" to call MakeAction.execute', () => {
+//         const parser = new Parser('2 + 3');
+//         const spy = jest
+//             .spyOn(parser, 'callMakeAction')
+//             .mockImplementation(mockCulc);
+//         expect(parser.execute()).toBe(5);
 
-    test('Parser set "  2 + 3  " to call MakeAction.execute', () => {
-        const parser = new Parser('2 + 3');
-        const spy = jest
-            .spyOn(parser, 'callMakeAction')
-            .mockImplementation(mockCulc);
-        expect(parser.execute()).toBe(5);
+//         spy.mockRestore();
+//     });
 
-        spy.mockRestore();
-    });
+//     test('Parser set "  2 + 3  " to call MakeAction.execute', () => {
+//         const parser = new Parser('2 + 3');
+//         const spy = jest
+//             .spyOn(parser, 'callMakeAction')
+//             .mockImplementation(mockCulc);
+//         expect(parser.execute()).toBe(5);
 
-    test('Parser set "2 + 3 + 3" to call MakeAction.execute', () => {
-        const parser = new Parser('2 + 3 + 3');
+//         spy.mockRestore();
+//     });
+
+//     test('Parser set "2 + 3 + 3" to call MakeAction.execute', () => {
+//         const parser = new Parser('2 + 3 + 3');
+//         const spy = jest
+//             .spyOn(parser, 'callMakeAction')
+//             .mockImplementationOnce(mockCulc);
+
+//         expect(parser.execute()).toBe(8);
+
+//         spy.mockRestore();
+//     });
+
+//     test('Parser set "2 + 3 + 3 + 1" to call MakeAction.execute', () => {
+//         const parser = new Parser('2 + 3 + 3 + 1');
+//         const spy = jest
+//             .spyOn(parser, 'callMakeAction')
+//             .mockImplementation(mockCulc);
+
+//         expect(parser.execute()).toBe(9);
+
+//         spy.mockRestore();
+//     });
+
+    test('Parser set "5 - 2 - 1" to call MakeAction.execute', () => {
+        const parser = new Parser('5 - 2 - 1');
         const spy = jest
             .spyOn(parser, 'callMakeAction')
             .mockImplementationOnce(mockCulc);
 
-        expect(parser.execute()).toBe(8);
-
-        spy.mockRestore();
-    });
-
-    test('Parser set "2 + 3 + 3 + 1" to call MakeAction.execute', () => {
-        const parser = new Parser('2 + 3 + 3 + 1');
-        const spy = jest
-            .spyOn(parser, 'callMakeAction')
-            .mockImplementation(mockCulc);
-
-        expect(parser.execute()).toBe(9);
+        expect(parser.execute()).toBe(hode);
 
         spy.mockRestore();
     });
