52f2f9f0c1130b2603899b8a7934614d878a2e99
d.plushchenkov <d.plushchenkov@notamedia.ru>
2023-04-18 21:33:19 +0300

    add Button.tsx

--- src/Components/Button.stories.tsx
+++ src/Components/Button.stories.tsx
@@ -5,38 +5,16 @@ import { Button } from './Button';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-    title: 'Example/Button',
+    title: 'Calculator/Button',
     component: Button,
-    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-    argTypes: {
-        backgroundColor: { control: 'color' },
-    },
 // eslint-disable-next-line prettier/prettier
 } as ComponentMeta<typeof Button>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-    primary: true,
-    label: 'Button',
+    label: '2',
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-    label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-    size: 'large',
-    label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-    size: 'small',
-    label: 'Button',
-};
--- src/Components/Calculator.tsx
+++ src/Components/Calculator.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import { Button } from './Button';
 
 export const Calculator = () => {
-
     return (
         <div>
-            <div className="status">{status}</div>
-            <div className="board-row">
-                первая строка цифр
+            <div className="buttons-row">
+                <Button label={'7'} />
+                <Button label={'8'} />
+                <Button label={'9'} />
             </div>
-            <div className="board-row">
-                вторая строка цифр
+            <div className="buttons-row">
+                <Button label={'4'} />
+                <Button label={'5'} />
+                <Button label={'6'} />
             </div>
-            <div className="board-row">
-                третья строка цифр
+            <div className="buttons-row">
+                <Button label={'1'} />
+                <Button label={'2'} />
+                <Button label={'3'} />
             </div>
         </div>
     );
