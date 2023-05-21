f8b82b05d5fdcca35380bb7af3440cac17c8000b
d.plushchenkov <d.plushchenkov@notamedia.ru>
2023-05-21 18:11:27 +0300

    fix quotes in style

--- src/Components/Calculator.tsx
+++ src/Components/Calculator.tsx
@@ -4,17 +4,17 @@ import { Button } from './Button';
 export const Calculator = () => {
     return (
         <div>
-            <div className="buttons-row">
+            <div className='buttons-row'>
                 <Button label={'7'} />
                 <Button label={'8'} />
                 <Button label={'9'} />
             </div>
-            <div className="buttons-row">
+            <div className='buttons-row'>
                 <Button label={'4'} />
                 <Button label={'5'} />
                 <Button label={'6'} />
             </div>
-            <div className="buttons-row">
+            <div className='buttons-row'>
                 <Button label={'1'} />
                 <Button label={'2'} />
                 <Button label={'3'} />
