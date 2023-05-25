3abeef0e52892eb19fa42d1219ce9c2fd4a0deb1
d.plushchenkov <d.plushchenkov@notamedia.ru>
2023-05-25 21:48:58 +0300

    add component with state

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Calculator } from './Components/Calculator';
+import { CalculatorAsClassWithState } from '@/Components/CalculatorAsClassWithState';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Calculator />
+        <CalculatorAsClassWithState />
     </React.StrictMode>
 );
