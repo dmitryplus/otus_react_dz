444da63b097641c784c3be25222bb9d4adf98a8f
d.plushchenkov <d.plushchenkov@notamedia.ru>
2023-06-25 18:19:26 +0300

    add protected routes

---
+++ src/Pages/Login.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+import { Authorization } from '../Components/Authorization';
+
+export const Login: FC = () => {
+    return (
+        <>
+            <div>Авторизация</div>
+            <Authorization />
+        </>
+    );
+};
