869efd12757bf153304488c2e565f34bb5ec194c
d.plushchenkov <d.plushchenkov@notamedia.ru>
2023-06-25 15:23:59 +0300

    update type

--- src/Types/xhprof.ts
+++ src/Types/xhprof.ts
@@ -1,9 +1,9 @@
 type xprofParams = {
-    ct: number,
-    wt: number,
-    cpu: number,
-    mu: number,
-    pmu: number,
+    ct: number, //количество вызовов в контексте запроса
+    wt: number, //walltime (microsecs)  время выполнения запроса
+    cpu: number, //cpu time (microsecs) время, затраченное процессором на обработку запроса
+    mu: number, //memory usage (bytes) изменение потребления памяти после вызова метода
+    pmu: number, //peak memory usage  (bytes) изменение пикового потребления памяти после вызова метода
 };
 
 export type Xhprof = { [name: string]: xprofParams };
