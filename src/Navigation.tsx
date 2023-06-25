2e8b48d7af5aa18c6aefd41f4a10ee2decb2cf9f
d.plushchenkov <d.plushchenkov@notamedia.ru>
2023-06-25 18:18:26 +0300

    add protected routes

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,24 +1,33 @@
 import React, { FC, ReactNode } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Auth } from './Pages/Auth';
+import { Login } from './Pages/Login';
 import { Main } from './Pages/Main';
 import { Catalog } from './Pages/Catalog';
 import { ComponentWithState } from './Components/ComponentWithState';
 import { NotFound } from './Pages/NotFound';
+import { ProtectedRoutes } from './ProtectedRoutes';
 
 interface NavigationProps {
     children: ReactNode;
 }
 
-export const Navigation: FC<NavigationProps> = ({ children }) => (
-    <BrowserRouter>
-        {children}
+const authElements = (
+    <ProtectedRoutes>
         <Routes>
-            <Route path='/auth' element={<Auth />} />
             <Route index element={<Main />} />
             <Route path='/catalog' element={<Catalog />} />
             <Route path='/xhprof' element={<ComponentWithState />} />
             <Route path='*' element={<NotFound />} />
+        </Routes>
+    </ProtectedRoutes>
+);
+
+export const Navigation: FC<NavigationProps> = ({ children }) => (
+    <BrowserRouter>
+        {children}
+        <Routes>
+            <Route path='/auth' element={<Login />} />
+            <Route path='*' element={authElements} />
         </Routes>
     </BrowserRouter>
 );
