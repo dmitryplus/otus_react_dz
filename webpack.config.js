1b9285bc3c2f23618ad42084e2842f0f4867c271
d.plushchenkov <d.plushchenkov@notamedia.ru>
2023-07-23 14:51:09 +0300

    add styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,85 +1,138 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require("copy-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const createPath = (dirName) => path.resolve(__dirname, dirName);
 
 module.exports = {
-	context: createPath('src'),
-	mode: 'development',
-	entry: './index.js',
-	output: {
-		filename: 'index.js',
-		path: createPath('docs'),
-	},
-	resolve: {
-        extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
-        alias: {
-            '@': createPath('src'),
-        }
+  context: createPath('src'),
+  mode: 'development',
+  entry: './index.tsx',
+  output: {
+    filename: `[name].js`,
+    path: createPath('dist'),
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    alias: {
+      '@': createPath('src'),
     },
-	devtool: "source-map",
-	devServer: {
-		historyApiFallback: true,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(jsx|tsx|ts|js)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: [
-							[
-								'@babel/preset-env',
-								{
-									targets:
-									{
-										node: 'current'
-									}
-								}
-							],
-							'@babel/preset-typescript',
-							'@babel/preset-react'
-						],
-						plugins: [
-							'@babel/plugin-proposal-class-properties'
-						]
-				  	}
-				}
-			}
-		],
-	  },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html'
-        }),
-        new CopyPlugin({
-            patterns: [
-               {
-                from: createPath('src/favicon.ico'),
-                to: createPath('docs'),
-            },
+  },
+  devtool: 'source-map',
+  devServer: {
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|tsx|ts|js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  targets: {
+                    node: 'current',
+                  },
+                },
+              ],
+              '@babel/preset-typescript',
+              '@babel/preset-react',
             ],
-           }),
-		new CopyPlugin({
-			patterns: [
-				{
-					from: createPath('src/Data'),
-					to: createPath('docs/Data'),
-				},
-			],
-		}),
-		new CopyPlugin({
-			patterns: [
-				{
-					from: createPath('src/files-list.json'),
-					to: createPath('docs'),
-				},
-			],
-		}),
-        new CleanWebpackPlugin(),
-	]
-}
+            plugins: ['@babel/plugin-proposal-class-properties'],
+          },
+        },
+      },
+      {
+        test: /\.s([aс])ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[name]_[local]-[hash:base64:5]',
+              },
+            },
+          },
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          'postcss-loader',
+          {
+            loader: 'less-loader',
+            options: {
+              sourceMap: true,
+              lessOptions: {
+                javascriptEnabled: true,
+                modifyVars: {
+                  'primary-color': '#1BA829',
+                  'border-radius-base': '4px',
+                },
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+      chunkFilename: 'css/[name].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: createPath('src/favicon.ico'),
+          to: createPath('dist'),
+        },
+      ],
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: createPath('src/Data'),
+          to: createPath('dist/Data'),
+        },
+      ],
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: createPath('src/files-list.json'),
+          to: createPath('dist'),
+        },
+      ],
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
