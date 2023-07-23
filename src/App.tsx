95cc6779e96a1a368753e592aba8281c9433a938
d.plushchenkov <d.plushchenkov@notamedia.ru>
2023-07-23 14:35:05 +0300

    save changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ import { Start } from './Pages/Start';
 import { store } from './Redux/store';
 import { Navigation } from './Navigation';
 
-// eslint-disable-next-line react/function-component-definition
-export const App: React.FC = () => (
-  <Provider store={store}>
-    <Navigation>
-      <Start />
-    </Navigation>
-  </Provider>
-);
+export function App() {
+  return (
+    <Provider store={store}>
+      <Navigation>
+        <Start />
+      </Navigation>
+    </Provider>
+  );
+}
