c35a832b7c8ca350336db62d0c08e15d905a309d
d.plushchenkov <d.plushchenkov@notamedia.ru>
2023-07-24 13:10:35 +0300

    update list

--- src/Components/FileList/FileList.tsx
+++ src/Components/FileList/FileList.tsx
@@ -1,12 +1,12 @@
 import React, { FC, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { resetData, updateFilesList } from 'src/Redux/files';
-import { ListElement } from 'src/Components/ListElement';
 import { FileDispatch, State } from 'src/Types';
-import { v4 as uuidv4 } from 'uuid';
 import s from './FileList.module.sass';
 
-import { Divider, List, Typography } from 'antd';
+import { Button, Divider, List, Space, Typography } from 'antd';
+import { GetLinkToGraph } from 'src/Services';
+import { Link } from 'react-router-dom';
 
 export const FileList: FC = () => {
   const filesList = useSelector((state: State) => state.files.files);
@@ -22,58 +22,20 @@ export const FileList: FC = () => {
     }
   }, [dispatch, filesList]);
 
-
-  const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-  ];
-
-
   return (
     <div className={s.root}>
-      {filesList.map((fileName: string) => (
-        <ListElement fileName={fileName} key={uuidv4()} />
-      ))}
-
-      {/*<>*/}
-      {/*  <Divider orientation="left">Default Size</Divider>*/}
-      {/*  <List*/}
-      {/*    header={<div>Header</div>}*/}
-      {/*    footer={<div>Footer</div>}*/}
-      {/*    bordered*/}
-      {/*    dataSource={data}*/}
-      {/*    renderItem={(item) => (*/}
-      {/*      <List.Item>*/}
-      {/*        <Typography.Text mark>[ITEM]</Typography.Text> {item}*/}
-      {/*      </List.Item>*/}
-      {/*    )}*/}
-      {/*  />*/}
-      {/*  <Divider orientation="left">Small Size</Divider>*/}
-      {/*  <List*/}
-      {/*    size="small"*/}
-      {/*    header={<div>Header</div>}*/}
-      {/*    footer={<div>Footer</div>}*/}
-      {/*    bordered*/}
-      {/*    dataSource={data}*/}
-      {/*    renderItem={(item) => <List.Item>{item}</List.Item>}*/}
-      {/*  />*/}
-      {/*  <Divider orientation="left">Large Size</Divider>*/}
-      {/*  <List*/}
-      {/*    size="large"*/}
-      {/*    header={<div>Header</div>}*/}
-      {/*    footer={<div>Footer</div>}*/}
-      {/*    bordered*/}
-      {/*    dataSource={data}*/}
-      {/*    renderItem={(item) => <List.Item>{item}</List.Item>}*/}
-      {/*  />*/}
-      {/*</>*/}
-
-
-
-
+      <List
+        size="large"
+        bordered
+        dataSource={filesList}
+        renderItem={(item) => (
+          <List.Item>
+            <Space direction="vertical">
+              <Link to={GetLinkToGraph(item)}>{item}</Link>
+            </Space>
+          </List.Item>
+        )}
+      />
     </div>
   );
 };
--- src/Screens/Start/Start.tsx
+++ src/Screens/Start/Start.tsx
@@ -3,6 +3,7 @@ import { Helmet } from 'react-helmet';
 import s from './Start.module.sass';
 import { FileList } from 'src/Components/FileList';
 import { Frame } from 'src/Components/Frame';
+import { Divider } from "antd";
 
 const Start: FC = () => (
   <div className={s.root}>
@@ -10,7 +11,7 @@ const Start: FC = () => (
       <Helmet>
         <title>Список файлов</title>
       </Helmet>
-      <h1>Список файлов</h1>
+      <Divider orientation="left">Список файлов</Divider>
       <FileList />
     </Frame>
   </div>
