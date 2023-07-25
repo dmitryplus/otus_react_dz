5ccce24f2ef9f945f5259d2f2ab36ac66a7bf29d
d.plushchenkov <d.plushchenkov@notamedia.ru>
2023-07-25 14:50:07 +0300

    add expect to xhprof tests

--- tests/Xhprof.test.ts
+++ tests/Xhprof.test.ts
@@ -6,9 +6,31 @@ import {
   getChildrenTable,
   getMetrics,
   getPossibleMetrics,
+  parseParentChild,
 } from '../src/Services';
+import { unserialize } from 'locutus/php/var';
 
 const emptyXhprof = { 'main()': { ct: 1, wt: 6, cpu: 0, mu: 824, pmu: 0 } };
+
+const oneChildXhprof = {
+  'main()==>FirstTestClass::getValue': { ct: 1000, wt: 2074, cpu: 3316, mu: 856, pmu: 0 },
+  'main()': { ct: 1, wt: 6955, cpu: 9964, mu: 1464, pmu: 0 },
+};
+
+//console.log(unserialize('a:4:{s:52:"SecondTestClass::getValue==>FirstTestClass::getValue";a:5:{s:2:"ct";i:1000;s:2:"wt";i:1902;s:3:"cpu";i:0;s:2:"mu";i:872;s:3:"pmu";i:0;}s:34:"main()==>SecondTestClass::getValue";a:5:{s:2:"ct";i:1000;s:2:"wt";i:6947;s:3:"cpu";i:6602;s:2:"mu";i:1472;s:3:"pmu";i:0;}s:33:"main()==>FirstTestClass::getValue";a:5:{s:2:"ct";i:1;s:2:"wt";i:2;s:3:"cpu";i:0;s:2:"mu";i:536;s:3:"pmu";i:0;}s:6:"main()";a:5:{s:2:"ct";i:1;s:2:"wt";i:10201;s:3:"cpu";i:9929;s:2:"mu";i:2680;s:3:"pmu";i:0;}}'));
+
+const twoChildXhprof = {
+  'SecondTestClass::getValue==>FirstTestClass::getValue': { ct: 1000, wt: 1572, cpu: 3328, mu: 872, pmu: 0 },
+  'main()==>SecondTestClass::getValue': { ct: 1000, wt: 5432, cpu: 6650, mu: 1472, pmu: 0 },
+  'main()': { ct: 1, wt: 7717, cpu: 6650, mu: 2080, pmu: 0 },
+};
+
+const repeatCallXhprof = {
+  'SecondTestClass::getValue==>FirstTestClass::getValue': { ct: 1000, wt: 1902, cpu: 0, mu: 872, pmu: 0 },
+  'main()==>SecondTestClass::getValue': { ct: 1000, wt: 6947, cpu: 6602, mu: 1472, pmu: 0 },
+  'main()==>FirstTestClass::getValue': { ct: 1, wt: 2, cpu: 0, mu: 536, pmu: 0 },
+  'main()': { ct: 1, wt: 10201, cpu: 9929, mu: 2680, pmu: 0 },
+};
 
 describe('Xhprof', () => {
   test('buildParentChildKey', async () => {
@@ -34,9 +56,20 @@ describe('Xhprof', () => {
 
   test('computeInclusiveTimes', async () => {
     expect(computeInclusiveTimes(emptyXhprof)).toStrictEqual(emptyXhprof);
+
+    expect(computeInclusiveTimes(oneChildXhprof)).toStrictEqual({
+      'FirstTestClass::getValue': { ct: 1000, wt: 2074, cpu: 3316, mu: 856, pmu: 0 },
+      'main()': { ct: 1, wt: 6955, cpu: 9964, mu: 1464, pmu: 0 },
+    });
+
+    expect(computeInclusiveTimes(repeatCallXhprof)).toStrictEqual({
+      'FirstTestClass::getValue': { ct: 1001, wt: 1904, cpu: 0, mu: 1408, pmu: 0 },
+      'SecondTestClass::getValue': { ct: 1000, wt: 6947, cpu: 6602, mu: 1472, pmu: 0 },
+      'main()': { ct: 1, wt: 10201, cpu: 9929, mu: 2680, pmu: 0 },
+    });
   });
 
-  test('computeInclusiveTimes', async () => {
+  test('computeFlatInfo', async () => {
     expect(computeFlatInfo(emptyXhprof)).toStrictEqual({
       symTable: {
         'main()': {
@@ -53,10 +86,48 @@ describe('Xhprof', () => {
       },
       totals: { ct: 1, wt: 6, ut: 0, st: 0, cpu: 0, mu: 824, pmu: 0, samples: 0 },
     });
+
+    expect(computeFlatInfo(oneChildXhprof)).toStrictEqual({
+      symTable: {
+        'FirstTestClass::getValue': {
+          ct: 1000,
+          wt: 2074,
+          cpu: 3316,
+          mu: 856,
+          pmu: 0,
+          excl_wt: 2074,
+          excl_cpu: 3316,
+          excl_mu: 856,
+          excl_pmu: 0,
+        },
+        'main()': {
+          ct: 1,
+          wt: 6955,
+          cpu: 9964,
+          mu: 1464,
+          pmu: 0,
+          excl_wt: 6955,
+          excl_cpu: 9964,
+          excl_mu: 1464,
+          excl_pmu: 0,
+        },
+      },
+      totals: {
+        ct: 1001,
+        wt: 6955,
+        ut: 0,
+        st: 0,
+        cpu: 9964,
+        mu: 1464,
+        pmu: 0,
+        samples: 0,
+      },
+    });
   });
 
   test('getChildrenTable', async () => {
     expect(getChildrenTable(emptyXhprof)).toStrictEqual({});
+    expect(getChildrenTable(oneChildXhprof)).toStrictEqual({ 'main()': ['FirstTestClass::getValue'] });
   });
 
   test('generateDotScript', async () => {
@@ -66,5 +137,14 @@ describe('Xhprof', () => {
       '\n}';
 
     expect(generateDotScript(emptyXhprof)).toBe(needResult);
+  });
+
+  test('parseParentChild', async () => {
+    expect(parseParentChild('main()')).toStrictEqual({ child: 'main()', parent: null });
+
+    expect(parseParentChild('main()==>Nota\\Mentors\\Rest\\Order::composeReschedules')).toStrictEqual({
+      parent: 'main()',
+      child: 'Nota\\Mentors\\Rest\\Order::composeReschedules',
+    });
   });
 });
